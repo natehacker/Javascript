4a2bd0e4c7c0ea262bbf657da1a0d6a13f643376
Nathan louissaint <louissaintmark@gmail.com>
2023-03-17 17:42:37 -0400

    javascript basics

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// Basics Of Javascript
+//Basic Syntax
+x = 5;
+y = 6;
+let z = x + y;
+console.log(z)
+//JavaScript Operators
+//The Assignment Operator (=) assigns a value to a variable.
+let Q = 10;
+//The Addition Operator (+) adds numbers
+//The Multiplication Operator (*) multiplies numbers.
+//Arithmetic Operators are used to perform arithmetic on numbers:
+let a = 3;
+let w = (100 + 50) * a;
+console.log(w)
+// javascript Data Types 
+/*JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object 8
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date*/
